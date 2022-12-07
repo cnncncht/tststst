c92ea8ab4e0726d04bf7368130aa6f8e54483652
ivesfurtado <119992727+cnncncht@users.noreply.github.com>
2022-12-07 00:44:00 -0300

    chore: init

---
+++ src/components/Swap/index.tsx
@@ -0,0 +1,71 @@
+import Wallet from 'components/ConnectWallet'
+import { SwapInfoProvider } from 'hooks/swap/useSwapInfo'
+import useSyncController, { SwapController } from 'hooks/swap/useSyncController'
+import useSyncConvenienceFee, { FeeOptions } from 'hooks/swap/useSyncConvenienceFee'
+import useSyncSwapEventHandlers, { SwapEventHandlers } from 'hooks/swap/useSyncSwapEventHandlers'
+import useSyncTokenDefaults, { TokenDefaults } from 'hooks/swap/useSyncTokenDefaults'
+import { usePendingTransactions } from 'hooks/transactions'
+import { useAtom } from 'jotai'
+import { useMemo, useState } from 'react'
+import { displayTxHashAtom } from 'state/swap'
+
+import Dialog from '../Dialog'
+import Header from '../Header'
+import { BoundaryProvider } from '../Popover'
+import Input from './Input'
+import Output from './Output'
+import ReverseButton from './ReverseButton'
+import Settings from './Settings'
+import MaxSlippageSelect from './Settings/MaxSlippageSelect'
+import { StatusDialog } from './Status'
+import SwapActionButton from './SwapActionButton'
+import Toolbar from './Toolbar'
+import useValidate from './useValidate'
+
+// SwapProps also currently includes props needed for wallet connection (eg hideConnectionUI),
+// since the wallet connection component exists within the Swap component.
+// TODO(zzmp): refactor WalletConnection into Widget component
+export interface SwapProps extends FeeOptions, SwapController, SwapEventHandlers, TokenDefaults {
+  hideConnectionUI?: boolean
+  routerUrl?: string
+}
+
+export default function Swap(props: SwapProps) {
+  useValidate(props)
+  useSyncController(props as SwapController)
+  useSyncConvenienceFee(props as FeeOptions)
+  useSyncSwapEventHandlers(props as SwapEventHandlers)
+  useSyncTokenDefaults(props as TokenDefaults)
+
+  const [wrapper, setWrapper] = useState<HTMLDivElement | null>(null)
+
+  const [displayTxHash, setDisplayTxHash] = useAtom(displayTxHashAtom)
+  const pendingTxs = usePendingTransactions()
+  const displayTx = useMemo(() => displayTxHash && pendingTxs[displayTxHash], [displayTxHash, pendingTxs])
+
+  return (
+    <>
+      <Header>
+        <Wallet disabled={props.hideConnectionUI} />
+        <Settings />
+      </Header>
+      <div ref={setWrapper}>
+        <BoundaryProvider value={wrapper}>
+          <SwapInfoProvider routerUrl={props.routerUrl}>
+            <MaxSlippageSelect />
+            <Input />
+            <ReverseButton />
+            <Output />
+            <Toolbar />
+            <SwapActionButton />
+          </SwapInfoProvider>
+        </BoundaryProvider>
+      </div>
+      {displayTx && (
+        <Dialog color="dialog">
+          <StatusDialog tx={displayTx} onClose={() => setDisplayTxHash()} />
+        </Dialog>
+      )}
+    </>
+  )
+}
---
+++ src/hooks/useTokenAllowance.ts
@@ -0,0 +1,51 @@
+import { BigNumberish } from '@ethersproject/bignumber'
+import { CurrencyAmount, MaxUint256, Token } from '@uniswap/sdk-core'
+import { useSingleCallResult } from 'hooks/multicall'
+import { useCallback, useMemo } from 'react'
+import { ApprovalTransactionInfo, TransactionType } from 'state/transactions'
+import { calculateGasMargin } from 'utils/calculateGasMargin'
+
+import { useTokenContract } from './useContract'
+
+export function useTokenAllowance(token?: Token, owner?: string, spender?: string): CurrencyAmount<Token> | undefined {
+  const contract = useTokenContract(token?.address, false)
+
+  const inputs = useMemo(() => [owner, spender], [owner, spender])
+  const allowance = useSingleCallResult(contract, 'allowance', inputs).result
+
+  return useMemo(
+    () => (token && allowance ? CurrencyAmount.fromRawAmount(token, allowance.toString()) : undefined),
+    [token, allowance]
+  )
+}
+
+export function useUpdateTokenAllowance(amount?: CurrencyAmount<Token>, spender?: string) {
+  const contract = useTokenContract(amount?.currency.address)
+
+  return useCallback(async (): Promise<ApprovalTransactionInfo> => {
+    try {
+      if (!amount) throw new Error('missing amount')
+      if (!contract) throw new Error('missing contract')
+      if (!spender) throw new Error('missing spender')
+
+      let allowance: BigNumberish = MaxUint256.toString()
+      const estimatedGas = await contract.estimateGas.approve(spender, allowance).catch(() => {
+        // Fallback for tokens which restrict approval amounts:
+        allowance = amount.quotient.toString()
+        return contract.estimateGas.approve(spender, allowance)
+      })
+
+      const gasLimit = calculateGasMargin(estimatedGas)
+      const response = await contract.approve(spender, allowance, { gasLimit })
+      return {
+        type: TransactionType.APPROVAL,
+        response,
+        tokenAddress: contract.address,
+        spenderAddress: spender,
+      }
+    } catch (e: unknown) {
+      const symbol = amount?.currency.symbol ?? 'Token'
+      throw new Error(`${symbol} approval failed: ${e instanceof Error ? e.message : e}`)
+    }
+  }, [amount, contract, spender])
+}
